da75fea9d2a565e7d35b491168299655a8c198f7
Jared Steiner <steiner.jared1996@gmail.com>
2021-12-09 11:23:09 +1100

    Initial input reading setup

--- day-1-sonar-sweep/src/main.rs
+++ day-1-sonar-sweep/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap()]
+struct Cli {
+    #[clap()]
+    input: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let contents = fs::read_to_string(cli.input)
+        .expect("Something went wrong reading the file");
+
+    for content in contents.lines() {
+        println!(".{}", content)
+    }
 }
